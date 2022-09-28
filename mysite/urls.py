ee17c32be9228ec590012a9e3b295ef8c3a21b35
Satyarth Praveen <satyarth@lbl.gov>
2022-09-28 00:55:12 -0700

    Redirecting base URL to the retroapp.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('retroapp.urls')),
+    
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('dfshow/', include('dfshow.urls')),
